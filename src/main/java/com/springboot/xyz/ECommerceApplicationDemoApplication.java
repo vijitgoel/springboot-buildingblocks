96db95274bdf958391cc8968d981f017c5f811f5
Vijit.Goel <vijit.goel.ext@aubank.in>
2023-10-05 14:08:22 +0530

    First COmmit

---
+++ src/main/java/com/springboot/xyz/ECommerceApplicationDemoApplication.java
@@ -0,0 +1,14 @@
+package com.springboot.xyz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECommerceApplicationDemoApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(ECommerceApplicationDemoApplication.class, args);
+	}
+
+}
